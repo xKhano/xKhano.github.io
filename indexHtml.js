f515f37ac1ce4ffc5b059a5c91f7059cae321fd8
Kaan Dalkıran <72474584+xKhano@users.noreply.github.com>
2025-03-10 15:09:42 +0300

    Email Button

--- indexHtml.js
+++ indexHtml.js
@@ -76,6 +76,26 @@
                                     "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z",
                             }),
                         });
+                    case "email":
+                        return (0, s.jsx)("svg", {
+                            xmlns: "http://www.w3.org/2000/svg",
+                            viewBox: "0 0 24 24",
+                            fill: "none",
+                            className: a,
+                            strokeWidth:1.5,
+                            stroke:"currentColor",
+                            "aria-hidden": "true",
+                            children: [(0, s.jsx)("path", {
+                                d:
+                                    "M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z",
+                            }),
+                            (0, s.jsx)("path", {
+                                d:
+                                    "M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z",
+                            })
+
+                    ],
+                    });
                     case "instagram":
                         return (0, s.jsx)("svg", {
                             xmlns: "http://www.w3.org/2000/svg",
@@ -609,7 +629,9 @@
                             "aria-label": "Social media",
                             children: [
                                 { title: "GitHub", url: "https://github.com/xKhano", icon: "github" },
-                                { title: "LinkedIn", url: "https://www.linkedin.com/in/there-are-no-little-details/", icon: "linkedin" }
+                                { title: "LinkedIn", url: "https://www.linkedin.com/in/there-are-no-little-details/", icon: "linkedin" },
+                                { title: "Email", url: "mailto:kaan51dlkrn@gmail.com", icon: "email" },
+                                { title: "WhatsApp", url: "mailto:kaan51dlkrn@gmail.com", icon: "whatsapp" }
                             ].map((e, t) =>
                                 (0, s.jsx)(
                                     "li",
